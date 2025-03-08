a8780d74a69d8b9dcc4006d84c920717883d916f
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-03-08 18:41:52 -0300

    adicionando consigurações de segurança

---
+++ src/main/java/com/yanvelasco/agendadortarefas/domain/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.yanvelasco.agendadortarefas.domain.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+}
