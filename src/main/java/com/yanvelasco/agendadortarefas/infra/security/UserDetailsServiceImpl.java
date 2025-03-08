b65aca298ff0cad51b27f2389f71f89e8990d284
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-03-08 18:52:37 -0300

    adicionando Response Entity

--- src/main/java/com/yanvelasco/agendadortarefas/infra/security/UserDetailsServiceImpl.java
+++ src/main/java/com/yanvelasco/agendadortarefas/infra/security/UserDetailsServiceImpl.java
@@ -4,8 +4,8 @@ import com.yanvelasco.agendadortarefas.domain.dto.UsuarioDTO;
 import com.yanvelasco.agendadortarefas.infra.client.UsuarioClient;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
 
 @Service
 public class UserDetailsServiceImpl {
@@ -17,7 +17,8 @@ public class UserDetailsServiceImpl {
     }
 
     public UserDetails carregaDadosDeUsuario(String email, String token) {
-        UsuarioDTO usuarioDTO = usuarioClient.buscaUsuarioPorEmail(token, email);
+        ResponseEntity<UsuarioDTO> responseEntity = usuarioClient.buscaUsuarioPorEmail(token, email);
+        UsuarioDTO usuarioDTO = responseEntity.getBody();
         return User
                 .withUsername(usuarioDTO.getEmail())
                 .password(usuarioDTO.getSenha())
